3b92bfb504b33a1d4bd3c257357d740971d42f02
AbyG1 <abygeorge133@gmail.com>
2024-01-21 23:59:36 +0530

    Add route

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Home from './Components/Home'
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer'
+import View from './Components/View';
 function App() {
   return (
     <div className="App">
         <Header></Header>
         <Routes>
             <Route path={'/'} element={<Home/>}></Route>
+            <Route path={'/view/:id'} element={<View/>}></Route>
             <Route path={'*'} element={<PageNotFound/>}></Route>
         </Routes>
         <Footer></Footer>
